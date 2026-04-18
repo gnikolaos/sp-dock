14adb496e11657874698d39e9eeeaaf0443da704
gnikolaos <dizars1776@yahoo.gr>
2026-04-18 20:18:17 +0300

    fix: use preferencesPage

--- prefs.js
+++ prefs.js
@@ -1,88 +1,230 @@
-// Copyright(C) 2021  Emre Şenliyim
-
-// This program is free software: you can redistribute it and / or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-// GNU General Public License for more details.
-
-//     You should have received a copy of the GNU General Public License
-// along with this program.If not, see < http://www.gnu.org/licenses/>.
-
-import GObject from "gi://GObject";
-import Gtk from "gi://Gtk";
+import Adw from "gi://Adw";
 import Gio from "gi://Gio";
+import Gtk from "gi://Gtk";
 
-import {
-    ExtensionPreferences,
-    gettext as _,
-} from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
+import { ExtensionPreferences, gettext as _ } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
 
 import settingsFields from "./settingsFields.js";
 
-const SpBuilderScope = GObject.registerClass(
-    {
-        Implements: [Gtk.BuilderScope],
-    },
-    class extends GObject.Object {
-        constructor(settings) {
-            super();
-            this.settings = settings;
-            settingsFields.forEach((field) => {
-                if (!field.resettable) {
-                    return;
-                }
-                this[field.resetCallback] = (connectObject) => {
-                    settings.reset(field.setting);
-                };
-            });
-        }
-
-        vfunc_create_closure(builder, handlerName, flags, connectObject) {
-            if (flags & Gtk.BuilderClosureFlags.SWAPPED) {
-                throw new Error('Unsupported template signal flag "swapped"');
-            }
-            if (typeof this[handlerName] === "undefined") {
-                throw new Error(`${handlerName} is undefined`);
-            }
-            return this[handlerName].bind(connectObject || this);
-        }
-
-        resetAllToDefault() {
-            settingsFields.forEach((field) => {
-                if (field.resettable) this.settings.reset(field.setting);
-            });
-        }
-
-        on_defaults_clicked(connectObject) {
-            this.resetAllToDefault();
-        }
-    },
-);
-
 export default class SpDockPrefs extends ExtensionPreferences {
     fillPreferencesWindow(window) {
-        window._settings = this.getSettings();
-        const builder = new Gtk.Builder();
+        const settings = this.getSettings();
+        window._settings = settings;
 
-        // builder.set_scope(new RegisteredClass(settings));
-        builder.set_scope(new SpBuilderScope(settings));
-        builder.add_from_file(this.path + "/prefs.xml");
+        const page = new Adw.PreferencesPage();
+        window.add(page);
 
-        // bind switches and text fields to their respective settings
-        settingsFields.forEach((field) =>
-            settings.bind(
-                field.setting,
-                builder.get_object(field.fieldId),
-                field.type,
-                Gio.SettingsBindFlags.DEFAULT,
-            ),
-        );
+        const displayGroup = new Adw.PreferencesGroup({
+            title: _("Main settings"),
+        });
+        page.add(displayGroup);
 
-        window.add(builder.get_object("prefs_widget"));
+        const formatRow = new Adw.EntryRow({
+            title: _("Display format for songs"),
+        });
+        settings.bind("display-format", formatRow, "text", Gio.SettingsBindFlags.DEFAULT);
+        displayGroup.add(formatRow);
+
+        const podcastFormatRow = new Adw.EntryRow({
+            title: _("Display format for podcasts"),
+        });
+        settings.bind("podcast-format", podcastFormatRow, "text", Gio.SettingsBindFlags.DEFAULT);
+        displayGroup.add(podcastFormatRow);
+
+        const displayModeRow = new Adw.ComboRow({
+            title: _("Display mode"),
+            model: new Gtk.StringList(),
+        });
+        displayModeRow.model.append(_("Static"));
+        displayModeRow.model.append(_("Marquee"));
+        settings.bind("display-mode", displayModeRow, "selected", Gio.SettingsBindFlags.DEFAULT);
+        displayGroup.add(displayModeRow);
+
+        const positionRow = new Adw.ComboRow({
+            title: _("Position in panel"),
+            model: new Gtk.StringList(),
+        });
+        positionRow.model.append(_("Left"));
+        positionRow.model.append(_("Center"));
+        positionRow.model.append(_("Right"));
+        settings.bind("position", positionRow, "selected", Gio.SettingsBindFlags.DEFAULT);
+        displayGroup.add(positionRow);
+
+        const logoPositionRow = new Adw.ComboRow({
+            title: _("Show Spotify logo"),
+            model: new Gtk.StringList(),
+        });
+        logoPositionRow.model.append(_("Don't show"));
+        logoPositionRow.model.append(_("Left of label"));
+        logoPositionRow.model.append(_("Right of label"));
+        settings.bind("logo-position", logoPositionRow, "selected", Gio.SettingsBindFlags.DEFAULT);
+        displayGroup.add(logoPositionRow);
+
+        const staticGroup = new Adw.PreferencesGroup({
+            title: _("Static display settings"),
+        });
+        page.add(staticGroup);
+
+        const titleLengthRow = new Adw.SpinRow({
+            title: _("Max. title field length"),
+            adjustment: new Gtk.Adjustment({
+                lower: 5,
+                upper: 200,
+                step_increment: 1,
+            }),
+        });
+        settings.bind("title-max-length", titleLengthRow, "value", Gio.SettingsBindFlags.DEFAULT);
+        staticGroup.add(titleLengthRow);
+
+        const artistLengthRow = new Adw.SpinRow({
+            title: _("Max. artist field length"),
+            adjustment: new Gtk.Adjustment({
+                lower: 5,
+                upper: 200,
+                step_increment: 1,
+            }),
+        });
+        settings.bind("artist-max-length", artistLengthRow, "value", Gio.SettingsBindFlags.DEFAULT);
+        staticGroup.add(artistLengthRow);
+
+        const albumLengthRow = new Adw.SpinRow({
+            title: _("Max. album field length"),
+            adjustment: new Gtk.Adjustment({
+                lower: 5,
+                upper: 200,
+                step_increment: 1,
+            }),
+        });
+        settings.bind("album-max-length", albumLengthRow, "value", Gio.SettingsBindFlags.DEFAULT);
+        staticGroup.add(albumLengthRow);
+
+        const marqueeGroup = new Adw.PreferencesGroup({
+            title: _("Marquee display settings"),
+        });
+        page.add(marqueeGroup);
+
+        const marqueeLengthRow = new Adw.SpinRow({
+            title: _("Max. marquee length"),
+            adjustment: new Gtk.Adjustment({
+                lower: 5,
+                upper: 200,
+                step_increment: 1,
+            }),
+        });
+        settings.bind("marquee-length", marqueeLengthRow, "value", Gio.SettingsBindFlags.DEFAULT);
+        marqueeGroup.add(marqueeLengthRow);
+
+        const marqueeIntervalRow = new Adw.SpinRow({
+            title: _("Marquee frame duration in milliseconds"),
+            adjustment: new Gtk.Adjustment({
+                lower: 100,
+                upper: 20000,
+                step_increment: 50,
+            }),
+        });
+        settings.bind("marquee-interval", marqueeIntervalRow, "value", Gio.SettingsBindFlags.DEFAULT);
+        marqueeGroup.add(marqueeIntervalRow);
+
+        const marqueeTailRow = new Adw.EntryRow({
+            title: _("Marquee separator"),
+        });
+        settings.bind("marquee-tail", marqueeTailRow, "text", Gio.SettingsBindFlags.DEFAULT);
+        marqueeGroup.add(marqueeTailRow);
+
+        const inactiveGroup = new Adw.PreferencesGroup({
+            title: _("Inactive Spotify settings"),
+        });
+        page.add(inactiveGroup);
+
+        const hideInactiveRow = new Adw.SwitchRow({
+            title: _("Hide widget when Spotify is inactive"),
+        });
+        settings.bind("hidden-when-inactive", hideInactiveRow, "active", Gio.SettingsBindFlags.DEFAULT);
+        inactiveGroup.add(hideInactiveRow);
+
+        const notRunningRow = new Adw.EntryRow({
+            title: _("Text to show when inactive"),
+        });
+        settings.bind("off", notRunningRow, "text", Gio.SettingsBindFlags.DEFAULT);
+        inactiveGroup.add(notRunningRow);
+
+        const pausedGroup = new Adw.PreferencesGroup({
+            title: _("Pause playback settings"),
+        });
+        page.add(pausedGroup);
+
+        const hidePausedRow = new Adw.SwitchRow({
+            title: _("Hide widget when playback is paused"),
+        });
+        settings.bind("hidden-when-paused", hidePausedRow, "active", Gio.SettingsBindFlags.DEFAULT);
+        pausedGroup.add(hidePausedRow);
+
+        const pausedMetadataRow = new Adw.SwitchRow({
+            title: _("Show metadata when paused"),
+        });
+        settings.bind("metadata-when-paused", pausedMetadataRow, "active", Gio.SettingsBindFlags.DEFAULT);
+        pausedGroup.add(pausedMetadataRow);
+
+        const pausedTextRow = new Adw.EntryRow({
+            title: _("Text to show when paused"),
+        });
+        settings.bind("paused", pausedTextRow, "text", Gio.SettingsBindFlags.DEFAULT);
+        pausedGroup.add(pausedTextRow);
+
+        const stoppedGroup = new Adw.PreferencesGroup({
+            title: _("Stopped playback settings"),
+        });
+        page.add(stoppedGroup);
+
+        const hideStoppedRow = new Adw.SwitchRow({
+            title: _("Hide widget when playback is stopped"),
+        });
+        settings.bind("hidden-when-stopped", hideStoppedRow, "active", Gio.SettingsBindFlags.DEFAULT);
+        stoppedGroup.add(hideStoppedRow);
+
+        const stoppedTextRow = new Adw.EntryRow({
+            title: _("Text to show when stopped"),
+        });
+        settings.bind("stopped", stoppedTextRow, "text", Gio.SettingsBindFlags.DEFAULT);
+        stoppedGroup.add(stoppedTextRow);
+
+        const indicatorsGroup = new Adw.PreferencesGroup({
+            title: _("Loop and shuffle indicators"),
+        });
+        page.add(indicatorsGroup);
+
+        const shuffleRow = new Adw.EntryRow({
+            title: _("Shuffle"),
+        });
+        settings.bind("shuffle", shuffleRow, "text", Gio.SettingsBindFlags.DEFAULT);
+        indicatorsGroup.add(shuffleRow);
+
+        const loopPlaylistRow = new Adw.EntryRow({
+            title: _("Loop playlist"),
+        });
+        settings.bind("loop-playlist", loopPlaylistRow, "text", Gio.SettingsBindFlags.DEFAULT);
+        indicatorsGroup.add(loopPlaylistRow);
+
+        const loopTrackRow = new Adw.EntryRow({
+            title: _("Loop track"),
+        });
+        settings.bind("loop-track", loopTrackRow, "text", Gio.SettingsBindFlags.DEFAULT);
+        indicatorsGroup.add(loopTrackRow);
+
+        const actionsGroup = new Adw.PreferencesGroup();
+        page.add(actionsGroup);
+
+        const resetButtonRow = new Adw.ActionRow({
+            title: _("Restore defaults"),
+        });
+        resetButtonRow.set_activatable(true);
+        resetButtonRow.connect("activated", () => {
+            settingsFields.forEach((field) => {
+                if (field.resettable) {
+                    settings.reset(field.setting);
+                }
+            });
+        });
+        actionsGroup.add(resetButtonRow);
     }
 }
